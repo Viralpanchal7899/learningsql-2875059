c1e2155f1fc6fc385e2c73387c1fc1830d1f33ac
Viral Panchal <53462443+Viralpanchal7899@users.noreply.github.com>
2025-07-04 18:01:14 +0000

    course notes and SQL coding tips

--- working.sql
+++ working.sql
@@ -1,3 +1,4 @@
+-- CRUD = "" -- 
 
 -- -- "SELECT" --> Ability to read the data
 
@@ -54,4 +55,57 @@
 --   states
 -- WHERE 
 --   people.state_code=states.state_abbrev;
+
+-- -- "GROUP BY" --> dividing results in various ways
+
+-- SELECT first_name, COUNT(first_name)
+-- FROM people
+-- GROUP BY first_name;
+
+------------------------------------------
+-- Data types, math, and other features --
+------------------------------------------
+
+-- some built-in function available SUM(), AVG(), etc.
+-- Accepts math and logic operations in select or where statements
+
+-- -- Coumpound select {'Nested Select'}
+
+-- SELECT first_name, last_name, quiz_points
+-- FROM people
+-- WHERE quiz_points=(SELECT MAX(quiz_points) FROM people);
+
+-- -- Transformation of data
+
+-- LOWER(), UPPER() --> Controls the case of a string
+-- SUBSTR(X,Y,Z) --> short forming a string {eg; Timothy --> Tim; SUBSTR(name, 1,3)}
+-- REPLACE(X,Y,Z) --> replacing the elements of a string with something else
+-- CAST --> treat or change the data type {CAST(quiz_points[int] AS CHAR)}
+
+
+------------------------
+-- Add or Modify data --
+------------------------
+
+-- -- "INSERT" --> adding data to a table 
+-- INSERT INTO people
+-- (first_name, last_name)
+-- VALUES
+-- ('George', 'White'),
+-- ('Max', 'White'),
+-- ('George', 'Clooney');
+
+-- -- "UPDATE" --> updating the data in a table
+
+-- SELECT first_name, last_name
+-- FROM people
+-- UPDATE people SET last_name='Morrison' WHERE first_name='Carlos' AND city='Houston';
+
+-- SELECT first_name, last_name
+-- FROM people
+-- WHERE first_name='Carlos';
+
+-- -- "DELETE" --> remove a row of data from a table
+
+-- DELETE FROM people WHERE id_number=1001;
 
